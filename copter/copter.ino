c627f725f83d16a856240bc32bb52b77ae77c881
Ronnie-Leon76 <ronleon76@gmail.com>
2022-07-09 07:31:08 +0300

    leaner code

--- copter/copter.ino
+++ copter/copter.ino
@@ -18,24 +18,20 @@ LiquidCrystal_I2C lcd(0x27, 16 ,2);
 #define IN_ANGLE_MIN 0
 #define IN_ANGLE_MAX 90
 
-Servo ESC;     // create servo object to control the ESC
+Servo ESC;     
 
-int potValue;  // value from the analog pinsj
+int potValue;  
 int pwmPin = 9;
-int value;
-
-
+int value = 0;
 
 int read_angle(int potValue){
-  // Use Potentiometer to measure motor shaft angle
-  // Get the initial value of the potentiometer at 0 degrees and the potetiometer value at 90 degrees
   int angle = map(potValue, IN_ANALOG_READ_VAL_MIN, IN_ANALOG_READ_VAL_MAX, IN_ANGLE_MIN, IN_ANGLE_MAX);
   return angle;
 }
 
 int get_duty_cycle(int value){
-  int duty_cycle = map(value, 0, 1023, 0, 100);
-  return duty_cycle;
+  int duty_cycle = map(value, 0, 1023, 0, 5);
+  return duty_cycle/5*100;
 }
 
 void setup() {
@@ -45,64 +41,63 @@ void setup() {
 }
 
 void loop() {
-  potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
-  Serial.println(potValue);
-  lcd.setCursor(0,0);
-  lcd.print("The potentiometer reading is: ");
-  lcd.setCursor(1,0);
-  lcd.print(potValue);
-  lcd.clear();
-  int increment = 100;
-    
-    do{
+  do{
     ESC.write(value);
-    value+=increment;
+    value+=10;
     int duty_cycle = get_duty_cycle(value);
     int angle;
     if(duty_cycle == 20){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     }else if(duty_cycle == 40){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 50){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 60){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 70){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 80){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 90){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 100){
-        angle = read_angle(value);
+        potValue = analogRead(A0);
+        angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
         delay(5000);
@@ -110,9 +105,9 @@ void loop() {
     } else {
       Serial.println("Invalid Duty Cycle");
     }
+    
+    
   }
   while(value<=1023);
-  
-  
-  
+
 }
