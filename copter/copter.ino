ae3afb2732668fc1d3d466c308ab0cff3a5cb1c8
Ronnie-Leon76 <ronleon76@gmail.com>
2022-07-18 22:04:50 +0300

    20khz pwm signal

--- copter/copter.ino
+++ copter/copter.ino
@@ -1,28 +1,20 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16 ,2);
-/**
- * Copter Propeller 
- * Operations done:
- * 1. Self calibration step during initialization that allows the system to automatically find the vertical position
- * i.e ensure the PWM duty cycle is marked when motor shaft is at 0 degrees.
- * Also make sure motor driver runs clockwise
- * 2. Include Servo Library because we need a 50Hz PWM signal.
- * 3. Create an object of the Servo class
- * 4. Declare pwmPin variable and assign PWM pin 9 to it
- * 1. 
-**/
 
 #include <Servo.h>
-#define IN_ANALOG_READ_VAL_MIN 0
-#define IN_ANALOG_READ_VAL_MAX 1023
-#define IN_ANGLE_MIN 0
-#define IN_ANGLE_MAX 90
 
 Servo ESC;     
 
-int potValue;  
+ 
 int pwmPin = 9;
 int value = 0;
+int potValue;
+
+#define IN_ANALOG_READ_VAL_MIN 0
+#define IN_ANALOG_READ_VAL_MAX 264
+#define IN_ANGLE_MIN 0
+#define IN_ANGLE_MAX 90
+
 
 int read_angle(int potValue){
   int angle = map(potValue, IN_ANALOG_READ_VAL_MIN, IN_ANALOG_READ_VAL_MAX, IN_ANGLE_MIN, IN_ANGLE_MAX);
@@ -34,65 +26,105 @@ int get_duty_cycle(int value){
   return duty_cycle/5*100;
 }
 
+
 void setup() {
+  ESC.attach(pwmPin,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  ESC.attach(pwmPin,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+ 
 }
 
 void loop() {
-  do{
-    ESC.write(value);
+   lcd.setCursor(0,0);
+   lcd.print("Hello World!");
+   lcd.clear();
+   do{
+    int val = map(value, 0, 1023, 0, 180);
+    ESC.write(val);
+    delay(2000);
     value+=10;
     int duty_cycle = get_duty_cycle(value);
     int angle;
     if(duty_cycle == 20){
         potValue = analogRead(A0);
+        Serial.print(potValue);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("20% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print("20%");
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     }else if(duty_cycle == 40){
         potValue = analogRead(A0);
+        Serial.print(potValue);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("40% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print("30%");
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 50){
         potValue = analogRead(A0);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("50% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 60){
         potValue = analogRead(A0);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("60% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 70){
         potValue = analogRead(A0);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("70% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 80){
         potValue = analogRead(A0);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("80% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 90){
         potValue = analogRead(A0);
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
+        lcd.print("90% duty cycle");
+        lcd.setCursor(1,0);
         lcd.print(angle);
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else if(duty_cycle == 100){
@@ -100,14 +132,22 @@ void loop() {
         angle = read_angle(potValue);
         lcd.setCursor(0,0);
         lcd.print(angle);
+        Serial.print(angle);
+        Serial.print("\n");
         delay(5000);
         lcd.clear();
     } else {
-      Serial.println("Invalid Duty Cycle");
+        potValue = analogRead(A0);
+        lcd.setCursor(0,0);
+        lcd.print(potValue);
+        Serial.print(potValue);
+        Serial.print("\n");
+        angle = read_angle(potValue);
     }
     
     
   }
   while(value<=1023);
 
+  
 }
---
+++ copter/copter_version_2/copter_version_2.ino
@@ -0,0 +1,117 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16 ,2);
+
+#define MIN_ANGLE 0
+#define MAX_ANGLE 90
+#define MIN_ANALOG_VALUE 0
+#define MAX_ANALOG_VALUE 262
+int pwmPin = 5;
+int potValue;
+int angle;
+
+void analogWriteSAH_Init( void )
+{
+  // Stop the timer while we muck with it
+
+  TCCR1B = (0 << ICNC1) | (0 << ICES1) | (0 << WGM13) | (0 << WGM12) | (0 << CS12) | (0 << CS11) | (0 << CS10);
+  
+  // Set the timer to mode 14...
+  //
+  // Mode  WGM13  WGM12  WGM11  WGM10  Timer/Counter Mode of Operation  TOP   Update of OCR1x at TOV1  Flag Set on
+  //              CTC1   PWM11  PWM10
+  // ----  -----  -----  -----  -----  -------------------------------  ----  -----------------------  -----------
+  // 14    1      1      1      0      Fast PWM                         ICR1  BOTTOM                   TOP
+  
+  // Set output on Channel A to...
+  //
+  // COM1A1  COM1A0  Description
+  // ------  ------  -----------------------------------------------------------
+  // 1       0       Clear OC1A/OC1B on Compare Match (Set output to low level).
+  
+  TCCR1A = 
+      (1 << COM1A1) | (0 << COM1A0) |   // COM1A1, COM1A0 = 1, 0
+      (0 << COM1B1) | (0 << COM1B0) | 
+      (1 << WGM11) | (0 << WGM10);      // WGM11, WGM10 = 1, 0
+  
+  // Set TOP to...
+  //
+  // fclk_I/O = 16000000
+  // N        = 1
+  // TOP      = 799
+  //
+  // fOCnxPWM = fclk_I/O / (N * (1 + TOP))
+  // fOCnxPWM = 16000000 / (1 * (1 + 799))
+  // fOCnxPWM = 16000000 / 800
+  // fOCnxPWM = 20000
+
+  ICR1 = 799;
+  
+  // Ensure the first slope is complete
+
+  TCNT1 = 0;
+  
+  // Ensure Channel B is irrelevant
+  
+  OCR1B = 0;
+  
+  // Ensure Channel A starts at zero / off
+  
+  OCR1A = 0;
+  
+  // We don't need no stinkin interrupts
+  
+  TIMSK1 = (0 << ICIE1) | (0 << OCIE1B) | (0 << OCIE1A) | (0 << TOIE1);
+
+  // Ensure the Channel A pin is configured for output
+  DDRB |= (1 << DDB1);
+
+  // Start the timer...
+  //
+  // CS12  CS11  CS10  Description
+  // ----  ----  ----  ------------------------
+  // 0     0     1     clkI/O/1 (No prescaling)
+
+  TCCR1B = 
+      (0 << ICNC1) | (0 << ICES1) | 
+      (1 << WGM13) | (1 << WGM12) |              // WGM13, WGM12 = 1, 1
+      (0 << CS12) | (0 << CS11) | (1 << CS10);
+}
+
+void analogWriteSAH( uint16_t value )
+{
+  if ( (value >= 0) && (value < 800) )
+  {
+    OCR1A = value;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  analogWriteSAH_Init();
+  lcd.init();
+  lcd.backlight();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int min_duty=0;min_duty<=100;min_duty+=10){
+    int val = map(min_duty, 0, 100, 0, 255);
+    analogWriteSAH(val);
+    lcd.setCursor(0,0);
+    lcd.print("Duty Cycle: ");
+    lcd.setCursor(1,0);
+    lcd.print(min_duty);
+    delay(5000);
+    lcd.clear();
+    potValue = analogRead(A0);
+    angle = map(potValue, MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, MIN_ANGLE, MAX_ANGLE);
+    lcd.setCursor(0,0);
+    lcd.print("Angle: ");
+    lcd.setCursor(1,0);
+    lcd.print(angle);
+    delay(5000);
+    lcd.clear(); 
+  }
+  
+}
